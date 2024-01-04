fb6522edb3e9eef46aa5abdd653488374b457bed
devster99@gmail.com <devster99@gmail.com>
2024-01-03 20:54:07 -0700

    Added routes to appbar with navigation functionality. Routes currently missing

--- src/components/ResponsiveAppBar/ResponsiveAppBar.js
+++ src/components/ResponsiveAppBar/ResponsiveAppBar.js
@@ -4,11 +4,15 @@ import AppBar from "@mui/material/AppBar";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import { useNavigate } from "react-router";
+import "./ResponsiveAppBar.css";
 
 const ResponsiveAppBar = () => {
   const navigate = useNavigate();
   const handleClick = () => {
     navigate("/login");
+  };
+  const handleNavigate = (route) => {
+    navigate(`${route}`);
   };
   return (
     <AppBar sx={{ bgcolor: "gray" }}>
@@ -17,9 +21,10 @@ const ResponsiveAppBar = () => {
         direction="row"
         justifyContent="space-between"
         alignItems="center"
-        padding={"10px"}
+        padding="10px"
       >
-        <Grid item xs={4}>
+        {/* Open Kitchen Logo */}
+        <Grid item>
           <Typography
             variant="h6"
             noWrap
@@ -36,6 +41,52 @@ const ResponsiveAppBar = () => {
           >
             OpenKitchen
           </Typography>
+        </Grid>
+        <Grid item>
+          <Grid container>
+            <Grid item>
+              {/* My Recipes */}
+              <Typography
+                className="navBarRoute"
+                variant="h6"
+                noWrap
+                component="a"
+                sx={{
+                  mr: 2,
+                  display: { xs: "none", md: "flex" },
+                  fontFamily: "monospace",
+                  fontWeight: 700,
+                  letterSpacing: ".3rem",
+                  color: "inherit",
+                  textDecoration: "none",
+                }}
+                onClick={() => handleNavigate("/user/recipes")}
+              >
+                My Recipes
+              </Typography>
+            </Grid>
+            <Grid item>
+              {/* My Kitchens */}
+              <Typography
+                className="navBarRoute"
+                variant="h6"
+                noWrap
+                component="a"
+                sx={{
+                  mr: 2,
+                  display: { xs: "none", md: "flex" },
+                  fontFamily: "monospace",
+                  fontWeight: 700,
+                  letterSpacing: ".3rem",
+                  color: "inherit",
+                  textDecoration: "none",
+                }}
+                onClick={() => handleNavigate("/kitchens")}
+              >
+                My Kitchens
+              </Typography>
+            </Grid>
+          </Grid>
         </Grid>
         <Grid item>
           <Button
